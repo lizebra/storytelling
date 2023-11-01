008da3263611803ada85207f6976c6baf8a9a3f9
lizebra <135719916+lizebra@users.noreply.github.com>
2023-11-01 16:22:10 +0200

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTONnbEt_b_9m-HCdb_DGCP-yTT1HtFP4aj35D_knfGQMSqRT9Hl4AFTKYryMV_pSRgtxYXIif-7ctz/pubhtml';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1-j_8NuutY1-s_vepX2Vx9T3Na_v7yRdPa1pZ7oGoWWA/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyAtJarj0kmrj8KxD8kHrTjwhsEMiCTwJ94';
